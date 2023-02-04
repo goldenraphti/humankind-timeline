f05a99b5c6976693bb0b7113ba8d8c514ad42357
Raphael Ferrand <goldenraphti@users.noreply.github.com>
2023-02-04 16:48:56 +0100

    init + timeline

---
+++ src/utils/editorSettings.ts
@@ -0,0 +1,3 @@
+export interface IEditorSettings {}
+
+export const editorSetttings: IEditorSettings = {};
---
+++ src/utils/timeline.ts
@@ -0,0 +1,178 @@
+export interface IEvent {
+  title: string;
+  dateStringBeforePresent?: string;
+  dateYearsBeforePresent?: number;
+  date?: Date;
+  modernHistory: boolean;
+  description?: string;
+}
+
+const referenceYear = 2023;
+
+export const timeline: IEvent[] = [
+  {
+    title: "Last common grandmother of humans and chimpanzees",
+    dateStringBeforePresent: "6 million",
+    dateYearsBeforePresent: 6000000,
+    modernHistory: false,
+  },
+  {
+    title: "Evolution of the genus Homo in Africa. First stone tools",
+    dateStringBeforePresent: "2.5 million",
+    dateYearsBeforePresent: 2500000,
+    modernHistory: false,
+  },
+  {
+    title:
+      "Humans spread from Africa to Eurasia. Evolution of different human species",
+    dateStringBeforePresent: "2 million",
+    dateYearsBeforePresent: 2000000,
+    modernHistory: false,
+  },
+  {
+    title: "Neanderthals evolve in Europe and the Middle East",
+    dateStringBeforePresent: "500 000",
+    dateYearsBeforePresent: 500000,
+    modernHistory: false,
+  },
+  {
+    title: "Daily usage of fire.",
+    dateStringBeforePresent: "300 000",
+    dateYearsBeforePresent: 300000,
+    modernHistory: false,
+  },
+  {
+    title: "Homo Sapiens evolves in East Africa",
+    dateStringBeforePresent: "200000 BC",
+    dateYearsBeforePresent: 200000,
+    modernHistory: false,
+  },
+  {
+    title:
+      "The Cognitive Revolution. Emergence of fictive language. Beginning of history. Sapiens spread out of Africa",
+    dateStringBeforePresent: "-70 000",
+    dateYearsBeforePresent: 70000,
+    modernHistory: false,
+  },
+  {
+    title: "Sapiens settle Australia. Extinction of Australian megafauna",
+    dateStringBeforePresent: "-45 000",
+    dateYearsBeforePresent: 45000,
+    modernHistory: false,
+  },
+  {
+    title: "Extinction of Neanderthals",
+    dateStringBeforePresent: "-30 000",
+    dateYearsBeforePresent: 30000,
+    modernHistory: false,
+  },
+  {
+    title: "Sapiens settle America. Extinction of American megafauna",
+    dateStringBeforePresent: "-16 000",
+    dateYearsBeforePresent: 16000,
+    modernHistory: false,
+  },
+  {
+    title:
+      "Extinction of Homo floresiensis. Homo sapiens the only surviving human species",
+    dateStringBeforePresent: "-13 000",
+    dateYearsBeforePresent: 13000,
+    modernHistory: false,
+  },
+  {
+    title:
+      "The Agricultural Revolution. Domestication of plants and animals. Permanent settlements",
+    dateStringBeforePresent: "-12 000",
+    dateYearsBeforePresent: 12000,
+    modernHistory: false,
+  },
+  {
+    title: "First kingdoms, script and money. Polytheistic religions",
+    dateStringBeforePresent: "-5000",
+    dateYearsBeforePresent: 5000,
+    modernHistory: false,
+  },
+  {
+    title: "First empire – the Akkadian Empire of Sargon",
+    dateStringBeforePresent: "-4250",
+    dateYearsBeforePresent: 4250,
+    modernHistory: false,
+  },
+  {
+    title: `Invention of coinage – a universal money.
+    The Persian Empire – a universal political order ‘for the benefit of all humans’.
+    Buddhism in India – a universal truth ‘to liberate all beings from suffering’.`,
+    dateStringBeforePresent: "-2500",
+    dateYearsBeforePresent: 2500,
+    modernHistory: false,
+  },
+  {
+    title:
+      "Han Empire in China. Roman Empire in the Mediterranean. Invention of Christianity",
+    dateStringBeforePresent: "-2000",
+    dateYearsBeforePresent: 2000,
+    modernHistory: false,
+  },
+  {
+    title: "Invention of Islam",
+    dateStringBeforePresent: "-1400",
+    dateYearsBeforePresent: 1400,
+    modernHistory: false,
+  },
+  {
+    title: `The Scientific Revolution. Humankind admits its ignorance and begins to acquire unprecedented power. Europeans begin to conquer America and the oceans. The entire planet becomes a single historical arena. The rise of capitalism`,
+    dateStringBeforePresent: "-500",
+    dateYearsBeforePresent: 500,
+    modernHistory: false,
+  },
+  {
+    title:
+      "The Industrial Revolution. Family and community are replaced by state and market. Massive extinction of plants and animals.",
+    dateStringBeforePresent: "-200",
+    dateYearsBeforePresent: 200,
+    modernHistory: false,
+  },
+  {
+    title:
+      "Humans transcend the boundaries of planet Earth. Nuclear weapons threaten the survival of humankind. Organisms are increasingly shaped by intelligent design rather than natural selection",
+    dateStringBeforePresent: "0",
+    dateYearsBeforePresent: 0,
+    modernHistory: false,
+  },
+  // {
+  //   title: "",
+  //   dateStringBeforePresent: "",
+  //   dateYearsBeforePresent: ,
+  //   modernHistory: false,
+  // },
+  // {
+  //   title: "",
+  //   dateStringBeforePresent: "",
+  //   dateYearsBeforePresent: ,
+  //   modernHistory: false,
+  // },
+  // {
+  //   title: "",
+  //   dateStringBeforePresent: "",
+  //   dateYearsBeforePresent: ,
+  //   modernHistory: false,
+  // },
+  // {
+  //   title: "",
+  //   dateStringBeforePresent: "",
+  //   dateYearsBeforePresent: ,
+  //   modernHistory: false,
+  // },
+  // {
+  //   title: "",
+  //   dateStringBeforePresent: "",
+  //   dateYearsBeforePresent: ,
+  //   modernHistory: false,
+  // },
+  // {
+  //   title: "",
+  //   dateStringBeforePresent: "",
+  //   dateYearsBeforePresent: ,
+  //   modernHistory: false,
+  // },
+];
